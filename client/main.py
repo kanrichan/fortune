c9c810ee7d5efd226a431e42009a3976152a133f
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-10-16 00:17:46 +0800

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,25 +1,55 @@
 import requests
+import base64
+import requests
 import hashlib
 import time
- 
-# 基于验证的key
-au_key = "test"
+import random
 
-# 基于验证的时间
-au_time = int(time.time())
+from nonebot import *
 
-# 将验证的key与时间合并成一个字符
-au_key_time = "%s|%s"%(au_key,au_time)
+bot = get_bot()
 
-# 将合并的字符进行MD5加密
-m = hashlib.md5()
-m.update(bytes(au_key_time,encoding='utf-8'))
-authkey = m.hexdigest()
+@on_command('运势', only_to_me = False)
+async def _(session: CommandSession):
+    user = session.event.user_id
+    group = session.event.group_id
 
-# 将生成加密的 KEY 与 时间传递至服务端
-url = "http://www.kanri.ml:10086/fortune.jpg"
-data = {"types":"诺亚幻想",'fromQQ':"test"}
-headers = {'authkey':authkey,'autime':str(au_time)}
+    if group == 12345678:
+        values = ["碧蓝幻想", "原神", "公主连结", "诺亚幻想", "虚拟偶像", "阴阳师", "车万", "李清歌"]
+        types = random.choice(values)
+        limit = "off"
+    else:
+        types = "诺亚幻想"
+        limit = "none"
 
-a = requests.post(url=url,data=data,headers=headers)
-print(a.text)
+    # 基于验证的key
+    au_key = "test"
+
+    # 基于验证的时间
+    au_time = int(time.time())
+
+    # 将验证的key与时间合并成一个字符
+    au_key_time = "%s|%s"%(au_key,au_time)
+
+    # 将合并的字符进行MD5加密
+    m = hashlib.md5()
+    m.update(bytes(au_key_time,encoding='utf-8'))
+    authkey = m.hexdigest()
+
+    # 将生成加密的 KEY 与 时间传递至服务端
+    
+    data = {"version":3,"types":types,"fromQQ":user,"ask":"运势","limit":limit}
+    headers = {'authkey':authkey,'autime':str(au_time)}
+
+    url = "http://127.0.0.1:8000/fortune"
+    fortune = requests.post(url=url,data=data,headers=headers)
+
+    url = "http://127.0.0.1:8000/fortune.jpg"
+    pic = requests.post(url=url,data=data,headers=headers)
+    
+    imgdata = pic.content
+    base64_data = base64.b64encode(imgdata).decode("utf-8")
+
+    message = f'[CQ:image,file=base64://{base64_data}]'
+    await session.send(message)
+
