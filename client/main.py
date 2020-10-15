3594ca00618b259fc960e917cbdd2b5cf5d9f176
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-10-15 23:46:30 +0800

    Upadte fortune

--- client/main.py
+++ client/main.py
@@ -10,17 +10,15 @@ au_time = int(time.time())
 
 # 将验证的key与时间合并成一个字符
 au_key_time = "%s|%s"%(au_key,au_time)
-print(au_key_time)
+
 # 将合并的字符进行MD5加密
 m = hashlib.md5()
 m.update(bytes(au_key_time,encoding='utf-8'))
 authkey = m.hexdigest()
-print(authkey)
-print(au_time)
 
 # 将生成加密的 KEY 与 时间传递至服务端
-url = "http://127.0.0.1:8000/fortune.jpg"
-data = {"types":"noah",'fromQQ':"test"}
+url = "http://www.kanri.ml:10086/fortune.jpg"
+data = {"types":"诺亚幻想",'fromQQ':"test"}
 headers = {'authkey':authkey,'autime':str(au_time)}
 
 a = requests.post(url=url,data=data,headers=headers)
