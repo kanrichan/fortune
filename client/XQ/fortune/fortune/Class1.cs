073da634d7cbdbaff1c653937f84a52e915ffb11
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-10-18 00:27:30 +0800

    Update Client XQ

--- client/XQ/fortune/fortune/Class1.cs
+++ client/XQ/fortune/fortune/Class1.cs
@@ -11,8 +11,9 @@ using XQ.Net.SDK.EventArgs;
 using XQ.Net.SDK;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
 
-[assembly: XQPlugin("fortune", "木理", "1.0.0", "运势")]
+[assembly: XQPlugin("fortune-运势", "木理", "1.0.3", "Lucky!")]
 
 namespace fortune
 {
@@ -23,7 +24,9 @@ namespace fortune
         {
             public string Client;
             public string Version;
+            public string Bot;
             public string Types;
+            public string FromGroup;
             public string FromQQ;
             public string Ask;
             public string Limit;
@@ -35,18 +38,137 @@ namespace fortune
             public string Au_time;
         }
 
+        public static JObject OpenJson(string path)
+        {
+            StreamReader streamReader = new StreamReader(path);
+            string text = streamReader.ReadToEnd();
+            JObject json = (JObject)JsonConvert.DeserializeObject(text);
+            return json;
+        }
+
+        public static void WriteJson(string path)
+        {
+            try
+            {
+                string json = @"{
+    '默认': {
+        '触发': '运势',
+        '回复': '少女祈祷中......',
+        '类型': '李清歌|碧蓝幻想|公主连结',
+        '限制': '全局'
+    },
+    '单个群设置填群号': {
+        '触发': '这里填关键词',
+        '回复': '这里填收到关键词的回复',
+        '类型': '这里是池子类型，多个池子用|分开',
+        '限制': '每天限制一张，可填 全局 池子 关'
+    },
+    '00000000': {
+        '触发': '抽签',
+        '回复': '少女折寿中......',
+        '类型': '车万',
+        '限制': '池子'
+    },
+    '1048452984': {
+        '触发': '运势测试',
+        '回复': '收到命令！',
+        '类型': '李清歌',
+        '限制': '关'
+    }
+}";
+                File.WriteAllText(path, json);
+                XQAPI.OutPutLog("[fortune-运势] 检测到本插件为首次运行");
+                XQAPI.OutPutLog("[fortune-运势] 配置已生成 " + path);
+                XQAPI.OutPutLog("[fortune-运势] 若有需求可自行修改配置");
+            }
+            catch
+            {
+                XQAPI.OutPutLog("默认配置写入失败，请到项目地址提交issue");
+            }
+        }
+
+        public static string ReadJson(JObject configJson, string section, string key, string def)
+        {
+            string result = "";
+            try
+            {
+                result = configJson[section][key].ToString();
+            }
+            catch
+            {
+                result = def;
+            }
+            return result;
+        }
+
         [EnableEvent]
         public static void enable(object sender, XQEventArgs e)
         {
-            XQAPI.OutPutLog("fortune-运势初始化完毕");
+            string configPath = Path.Combine(XQAPI.AppDir, "config.json");
+            if (!File.Exists(@configPath))
+            {
+                WriteJson(@configPath);
+            }
+            XQAPI.OutPutLog("[fortune-运势] 项目地址 https://github.com/Yiwen-Chan/fortune");
+            XQAPI.OutPutLog("[fortune-运势] 特别感谢 fz6m https://github.com/fz6m/nonebot-plugin/tree/master/CQVortune");
+            XQAPI.OutPutLog("[fortune-运势] 特别感谢 Lostdegree https://github.com/Lostdegree/Portune");
+            XQAPI.OutPutLog("[fortune-运势] 想自定义运势背景并共享可加QQ群 1048452984 ");
         }
 
         [GroupMsgEvent]
         public static void onGroupMsg(object sender, XQAppGroupMsgEventArgs e)
         {
-            if (e.Message.Text == "运势")
+            string text = e.Message.Text;
+            string robot = e.RobotQQ;
+            string group = e.FromGroup.Id;
+            string user = e.FromQQ.Id;
+
+            string configPath = Path.Combine(XQAPI.AppDir, "config.json");
+            JObject configJson = OpenJson(@configPath);
+
+            if (ReadJson(configJson, group, "触发", "null") == "null")
             {
-                e.FromGroup.SendMessage(e.RobotQQ, "少女祈祷中......");
+                group = "默认";
+            }
+            else if (ReadJson(configJson, group, "触发", "null") == "关")
+            {
+                return;
+            }
+            string trigger = ReadJson(configJson, group, "触发", "运势");
+            string reply = ReadJson(configJson, group, "回复", "少女祈祷中......");
+            string types = ReadJson(configJson, group, "类型", "李清歌");
+            string limit = ReadJson(configJson, group, "限制", "池子");
+
+            if (types.Contains("|"))
+            {
+                List<string> list = new List<string>(types.Split('|'));
+                int length = list.ToArray().Length;
+                Random ran = new Random();
+                int RandKey = ran.Next(0, length);
+                types = list[RandKey];
+            }
+
+            if (limit == "全局")
+            {
+                limit = "on";
+            }
+            else if (limit == "池子")
+            {
+                limit = "none";
+            }
+            else if (limit == "关")
+            {
+                limit = "off";
+            }
+            else
+            {
+                limit = "none";
+            }
+
+            if (text == trigger)
+            {
+                XQAPI.OutPutLog("[fortune-运势] 开始向服务器请求数据......");
+                e.FromGroup.SendMessage(e.RobotQQ, reply);
 
                 string aukey = "test";
                 string apifortune = "http://127.0.0.1:8000/fortune";
@@ -55,10 +177,12 @@ namespace fortune
                 FromDataParm fromDataParm;
                 fromDataParm.Client = "xq";
                 fromDataParm.Version = "3";
-                fromDataParm.Types = "李清歌";
-                fromDataParm.FromQQ = "test";
-                fromDataParm.Ask = "运势";
-                fromDataParm.Limit = "on";
+                fromDataParm.Bot = robot;
+                fromDataParm.Types = types;
+                fromDataParm.FromGroup = group;
+                fromDataParm.FromQQ = user;
+                fromDataParm.Ask = text;
+                fromDataParm.Limit = limit;
 
                 string autime = get_autime();
                 string authkey = get_authkey(aukey, autime);
@@ -68,15 +192,53 @@ namespace fortune
 
                 string fortuneJson = fortune(apifortune, fromDataParm, headerParm);
 
-                e.FromGroup.SendMessage(e.RobotQQ, fortuneJson);
                 JObject json = (JObject)JsonConvert.DeserializeObject(fortuneJson);
+
                 string code = json["code"].ToString();
+                string msg = json["msg"].ToString();
+                string info = json["info"].ToString();
+                string warn = json["warn"].ToString();
 
-                e.FromGroup.SendMessage(e.RobotQQ, code);
+                string message = "";
+                string picPath = Path.Combine(XQAPI.AppDir, "output.jpg");
 
-                pic(apipic, fromDataParm, headerParm);
-
-                e.FromGroup.SendMessage(e.RobotQQ, "完毕！");
+                if (code != "200")
+                {
+                    message += "[fortune-运势] 服务器失联中......";
+                    e.FromGroup.SendMessage(e.RobotQQ, message);
+                    return;
+                }
+                if (msg != "success")
+                {
+                    message += msg;
+                    e.FromGroup.SendMessage(e.RobotQQ, message);
+                }
+                if (warn != "")
+                {
+                    pic(apipic, fromDataParm, headerParm);
+                    message += warn;
+                    message += "[pic=" + picPath + "]";
+                    e.FromGroup.SendMessage(e.RobotQQ, message);
+                    return;
+                }
+                if (info != "")
+                {
+                    pic(apipic, fromDataParm, headerParm);
+                    if (notSend())
+                    {
+                        message += info;
+                    }
+                    message += "[pic=" + picPath + "]";
+                    e.FromGroup.SendMessage(e.RobotQQ, message);
+                    return;
+                }
+                if (code == "200")
+                {
+                    pic(apipic, fromDataParm, headerParm);
+                    message += "[pic=" + picPath + "]";
+                    e.FromGroup.SendMessage(e.RobotQQ, message);
+                    return;
+                }
             }
         }
         public static string get_autime()
@@ -94,9 +256,27 @@ namespace fortune
             byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(aukeytime));
             for (int i = 0; i < s.Length; i++)
             {
-                authkey = authkey + s[i].ToString("X").ToLower();
+                authkey = authkey + s[i].ToString("x2");
             }
             return authkey;
+        }
+
+        public static string note = "";
+
+        public static bool notSend()
+        {
+            string todayTime = DateTime.Now.ToShortDateString().ToString();
+            if (note == "")
+            {
+                note = todayTime;
+                return true;
+            }
+            else if (note != todayTime)
+            {
+                note = todayTime;
+                return true;
+            }
+            return false;
         }
 
         public static string fortune(string api, FromDataParm fromDataParm, HeaderParm headerParm)
@@ -104,7 +284,9 @@ namespace fortune
             string postString = "";
             postString += string.Format("client={0}", fromDataParm.Client);
             postString += string.Format("&version={0}", fromDataParm.Version);
+            postString += string.Format("&bot={0}", fromDataParm.Bot);
             postString += string.Format("&types={0}", fromDataParm.Types);
+            postString += string.Format("&fromGroup={0}", fromDataParm.FromGroup);
             postString += string.Format("&fromQQ={0}", fromDataParm.FromQQ);
             postString += string.Format("&ask={0}", fromDataParm.Ask);
             postString += string.Format("&limit={0}", fromDataParm.Limit);
@@ -118,16 +300,21 @@ namespace fortune
             webClient.Headers.Add("autime", headerParm.Au_time);
 
             byte[] responseData = webClient.UploadData(api, "POST", postData);
-            string srcString = Encoding.UTF8.GetString(responseData);
+            string path = Path.Combine(XQAPI.AppDir, "output.txt");
+            File.WriteAllBytes(@path, responseData);
 
+            string srcString = Encoding.UTF8.GetString(responseData);
             return srcString;
         }
+
         public static void pic(string api, FromDataParm fromDataParm, HeaderParm headerParm)
         {
             string postString = "";
             postString += string.Format("client={0}", fromDataParm.Client);
             postString += string.Format("&version={0}", fromDataParm.Version);
+            postString += string.Format("&bot={0}", fromDataParm.Bot);
             postString += string.Format("&types={0}", fromDataParm.Types);
+            postString += string.Format("&fromGroup={0}", fromDataParm.FromGroup);
             postString += string.Format("&fromQQ={0}", fromDataParm.FromQQ);
             postString += string.Format("&ask={0}", fromDataParm.Ask);
             postString += string.Format("&limit={0}", fromDataParm.Limit);
@@ -142,13 +329,7 @@ namespace fortune
 
             byte[] responseData = webClient.UploadData(api, "POST", postData);
             string path = Path.Combine(XQAPI.AppDir, "output.jpg");
-            XQAPI.OutPutLog(path);
-            if (File.Exists(path))
-            {
-                File.Delete(path);
-            }
-            File.Create(path);
-            File.WriteAllBytes(path, responseData);
+            File.WriteAllBytes(@path, responseData);
         }
     }
 }
