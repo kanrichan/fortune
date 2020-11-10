2c894db76a88b4d1562be162e123a806423ed803
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-11-11 02:03:06 +0800

    Update GO

---
+++ client/GO/fortune/utils.go
@@ -0,0 +1,45 @@
+package fortune
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func PathExecute() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		OutPutLog("[fortune-运势] 判断当前运行路径失败")
+	}
+	return dir + "\\"
+}
+
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+func ReadAllText(path string) string {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+func WriteAllText(path, text string) {
+	_ = ioutil.WriteFile(path, []byte(text), 0644)
+}
+
+func CreatePath(path string) error {
+	err := os.MkdirAll(path, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Int2Str(val int64) string {
+	str := strconv.FormatInt(val, 10)
+	return str
+}
